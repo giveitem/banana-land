edb051f16e422091b408c9d4d23e49fbb4ec18b9
giveitem <49624400+giveitem@users.noreply.github.com>
2023-08-31 16:46:02 -0700

    Full program to insert 100k rows

--- src/main/java/Interact.java
+++ src/main/java/Interact.java
@@ -1,19 +1,50 @@
+import com.github.javafaker.Faker;
 import org.apache.commons.lang3.RandomStringUtils;
-
+import java.math.BigInteger;
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.sql.*;
+import java.util.HashSet;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
 
 public class Interact {
-    public static void main(String[] args) throws SQLException {
-        interact();
+    private static final String INSERT_USERS_SQL = "INSERT INTO user_info" +
+            "  (id, password, username, first_name, last_name, email, is_staff, is_course_manager, is_org_manager) " +
+            "VALUES " +
+            " (?, ?, ?, ?, ?, ?, ?, ?, ?);";
+
+
+    public static byte[] getSHA(String input) throws NoSuchAlgorithmException
+    {
+        // Static getInstance method is called with hashing SHA
+        MessageDigest md = MessageDigest.getInstance("SHA-256");
+
+        // digest() method called
+        // to calculate message digest of an input
+        // and return array of byte
+        return md.digest(input.getBytes(StandardCharsets.UTF_8));
+    }
+    public static String toHexString(byte[] hash)
+    {
+        // Convert byte array into signum representation
+        BigInteger number = new BigInteger(1, hash);
+
+        // Convert message digest into hex value
+        StringBuilder hexString = new StringBuilder(number.toString(16));
+
+        // Pad with leading zeros
+        while (hexString.length() < 64)
+        {
+            hexString.insert(0, '0');
+        }
+
+        return hexString.toString();
     }
 
-
-    public static Connection interact() throws SQLException {
-
-        String results = "";
+    public static void connect() {
         Connection con = null;
-        Statement readStatement = null;
-        ResultSet resultSet = null;
         try {
             Class.forName("org.postgresql.Driver");
             String dbName = System.getenv("RDS_DB_NAME");
@@ -26,25 +57,61 @@ public class Interact {
             con = DriverManager.getConnection(jdbcUrl);
             System.out.println("Remote connection successful.");
 
-//            return con;
+            insert(con);
         }
-        catch (ClassNotFoundException | SQLException e) {
-            System.out.println(e.toString());
+        catch (ClassNotFoundException | SQLException  e) {
+            System.out.println(e);
         }
-
-
-        readStatement = con.createStatement(ResultSet.TYPE_SCROLL_INSENSITIVE, ResultSet.CONCUR_READ_ONLY);
-        resultSet = readStatement.executeQuery("SELECT * FROM auth_user;");
-        resultSet.first();
-        results = resultSet.getString("username");
-        System.out.println(results);
-        resultSet.close();
-        readStatement.close();
-        con.close();
-        RandomStringUtils randomStringUtils = new RandomStringUtils();
-        String randomString = randomStringUtils.randomAlphanumeric(10);
-        System.out.println(randomString);
-        return null;
 
     }
+    public static void insert(Connection con) {
+        HashSet<Integer> set = new HashSet<>();
+
+        User[] userArray = new User[100000];
+        Random rand = new Random();
+        try {
+            for (int i = 0; i < 100000; i++) {
+                Faker faker = new Faker();
+                String randomString = RandomStringUtils.randomAlphanumeric(10);
+                int randomId = rand.nextInt(10000000);
+                while (set.contains(randomId)){
+                    randomId = rand.nextInt(10000000);
+                }
+                set.add(randomId);
+                userArray[i] = new User(randomId, toHexString(getSHA(randomString)), randomString,
+                        faker.name().firstName(), faker.name().lastName(), faker.internet().emailAddress(), rand.nextBoolean(),
+                        rand.nextBoolean(), rand.nextBoolean());
+            }
+        }catch (NoSuchAlgorithmException e) {
+            System.out.println("Exception thrown for incorrect algorithm: " + e);
+        }
+
+            for (User user : userArray){
+                try {
+                    TimeUnit.MILLISECONDS.sleep(10);
+                    PreparedStatement st = con.prepareStatement(INSERT_USERS_SQL);
+                    st.setInt(1, user.getId());
+                    st.setString(2, user.getPassword());
+                    st.setString(3, user.getUsername());
+                    st.setObject(4, user.getFirst_name());
+                    st.setObject(5, user.getLast_name());
+                    st.setObject(6, user.getEmail());
+                    st.setBoolean(7, user.isIs_staff());
+                    st.setBoolean(8, user.isIs_course_manager());
+                    st.setBoolean(9, user.isIs_org_manager());
+                    st.executeUpdate();
+                    st.close();
+                } catch (SQLException |InterruptedException e) {
+                    System.out.println(e);
+                }
+            }
+
+
+
+    }
+
+    public static void main(String[] args) throws SQLException {
+        connect();
+    }
+
 }
---
+++ src/main/java/User.java
@@ -0,0 +1,96 @@
+public class User {
+    private int id;
+    private String password;
+    private String username;
+    private String first_name;
+    private String last_name;
+    private String email;
+    private boolean is_staff;
+    private boolean is_course_manager;
+    private boolean is_org_manager;
+
+    public User(int id, String password, String username, String first_name, String last_name, String email, boolean is_staff, boolean is_course_manager, boolean is_org_manager) {
+        this.id = id;
+        this.password = password;
+        this.username = username;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.email = email;
+        this.is_staff = is_staff;
+        this.is_course_manager = is_course_manager;
+        this.is_org_manager = is_org_manager;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public boolean isIs_staff() {
+        return is_staff;
+    }
+
+    public void setIs_staff(boolean is_staff) {
+        this.is_staff = is_staff;
+    }
+
+    public boolean isIs_course_manager() {
+        return is_course_manager;
+    }
+
+    public void setIs_course_manager(boolean is_course_manager) {
+        this.is_course_manager = is_course_manager;
+    }
+
+    public boolean isIs_org_manager() {
+        return is_org_manager;
+    }
+
+    public void setIs_org_manager(boolean is_org_manager) {
+        this.is_org_manager = is_org_manager;
+    }
+
+}
