73490b37908061e4c65c2261a573c9aa8d7a9e03
jane__ <janecching@gmail.com>
2023-09-05 13:44:31 -0700

    added 4th table

--- Frontend/demo/src/App.js
+++ Frontend/demo/src/App.js
@@ -2,10 +2,11 @@ import { useState } from 'react';
 import Alert from 'react-bootstrap/Alert';
 import './App.css';
 import AuroraTable from './components/AuroraTable';
+import CurrentTable from './components/CurrentTable';
 import DynamoTable from './components/DynamoTable';
 import S3Table from './components/S3Table';
 import S3Table1 from './components/S3Table1';
-import { getChangeDBResult } from './fetcher.js';
+import { getChangeDBResult, getCurrentTable } from './fetcher.js';
 
 function App() {
   const [message, setMessage] = useState('');
@@ -18,6 +19,18 @@ function App() {
       console.log(res);
     })
   }
+
+  const toggle = () => {
+    getCurrentTable().then((res) => {
+      console.log(res);
+    })
+  }
+
+  const refresh = () => { 
+    window.location.reload();
+
+  }
+
   return (
     <div className="App">
       {showAlert && (
@@ -28,6 +41,11 @@ function App() {
         </>
       )}
       <div className="Table-container">
+      <div className="Table">
+          <h2>Toggle Table</h2>
+          <button className="App-button" onClick={toggle}>Switch Table</button>
+          <CurrentTable />
+        </div>
         <div className="Table">
           <h2>AuroraDB</h2>
           <button className="App-button" onClick={load}>Update Aurora Table</button>
@@ -35,7 +53,7 @@ function App() {
         </div>
         <div className="Table">
           <h2>S3 List</h2>
-          <button className="App-button">Refresh List</button>
+          <button className="App-button" onClick={refresh}>Refresh List</button>
           <p>us-west-1</p>
           <S3Table />
           <p>us-east-2</p>
